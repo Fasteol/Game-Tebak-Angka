402b5fab43a94470f37cbb68a46d314c9311275c
Fasteol <razanmfs507@gmail.com>
2026-05-28 16:56:21 +0700

    commit pertama: membuat game tebak angka menggunakan html, css, dan js

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const inputData = document.getElementById("angka")
+const btnConfirm = document.getElementById("submit")
+
+const info = document.getElementById("info")
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const angka = getRandomInt(1, 100)
+
+btnConfirm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const inputUser = inputData.value
+
+    if(inputUser > angka) {
+        info.innerHTML = "Angka terlalu tinggi"
+    } else if(inputUser < angka) {
+        info.innerHTML = "Angka terlalu rendah"
+    } else if(inputUser == angka) {
+        info.innerHTML = "Selamat, angka yang anda tebak benar!!!"
+        info.style.color = "green"
+    }
+})
+
